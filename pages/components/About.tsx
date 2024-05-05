8ea5cf63bd43096b57689c15e31bedf5ab33b4e5
Marko <mav3ricktv@gmail.com>
2024-05-05 02:27:14 +0200

    Svg path fixes

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -64,9 +64,9 @@ export default function About() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m9 5 7 7-7 7"
                 />
               </svg>
@@ -88,9 +88,9 @@ export default function About() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m9 5 7 7-7 7"
                 />
               </svg>
@@ -112,9 +112,9 @@ export default function About() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m9 5 7 7-7 7"
                 />
               </svg>
