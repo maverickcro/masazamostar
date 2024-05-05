20417e0e9babc2800e0bfb3f4296ce66c0ed9199
Marko <mav3ricktv@gmail.com>
2024-05-05 15:48:58 +0200

    Added contact button

--- pages/components/Cjenik.tsx
+++ pages/components/Cjenik.tsx
@@ -103,6 +103,24 @@ export default function Cjenik() {
               *Imam svoj prostor za masažu. U slučaju da želite dolazak na
               adresu to se naplaćuje dodatno.
             </span>
+            <a
+              href="#kontakt"
+              className="card-shadow inline-flex items-center justify-center px-5 py-3 mr-3 mt-2 w-[200px] text-base font-medium text-center text-white rounded-lg gradient-pozadina hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            >
+              Kontakt
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
         <div className="lg:col-span-6">
