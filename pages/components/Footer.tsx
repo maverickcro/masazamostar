5338492955e460832954fb608a8ee581b229ffcd
Marko <mav3ricktv@gmail.com>
2024-05-14 00:37:04 +0200

    Footer dizajn

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useEffect } from "react";
 import Image from "next/image";
+import { FaInstagram, FaTiktok } from "react-icons/fa";
 
 export default function Footer() {
   const useSmoothScroll = () => {
@@ -37,14 +38,24 @@ export default function Footer() {
       <div className="w-full max-w-4xl mx-auto p-4 md:py-8 text-center">
         <div className="flex text-center justify-between">
           <Image src="/logo.png" width={120} height={100} alt="logo" />
-          <Link
-            href="#nula"
-            className="space-x-3 rtl:space-x-reverse my-[auto] mx-0"
-          >
-            <span className="text-base font-semibold">
-              WEB STRANICA U IZRADI 🛠️
-            </span>
-          </Link>
+          <div className="flex space-x-4">
+            <a
+              href="https://www.masaza-mostar.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-3 rounded-full shadow-lg bg-gradient-to-bl from-[#e66465] to-[#9198e5]"
+            >
+              <FaInstagram className="text-white text-2xl" />
+            </a>
+            <a
+              href="https://www.tiktok.com/@alex_and_coco?_t=8mFwI38ryju&_r=1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-3 rounded-full shadow-lg bg-gradient-to-bl from-[#e66465] to-[#9198e5]"
+            >
+              <FaTiktok className="text-white text-2xl" />
+            </a>
+          </div>
         </div>
         <hr className="my-6 border-gray-400 sm:mx-auto lg:my-8" />
         <span className="block text-sm text-black sm:text-center dark:text-gray-400">
